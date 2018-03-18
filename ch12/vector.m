5bc604fe7b4eb6341663ba09e471927354000c77
Larry Jones <larry.jones@reveal-energy.com>
2018-03-18 16:27:15 -0500

    Simple vector with constructor
    This constructor fails when invoked as a default constructor

--- ch12/vector.m
+++ ch12/vector.m
@@ -1,6 +1,13 @@
 % A simple vector class
 
 classdef vector
+	methods
+		function this = vector(a, b)
+			this.x = a;
+			this.y = b;
+		end
+	end
+
 	properties
 		x; % x-value of vector
 		y; % y-value of vector
