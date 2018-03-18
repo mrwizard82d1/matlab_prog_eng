036560370c69be92c9eeb849ccf6bf1400340e22
Larry Jones <larry.jones@reveal-energy.com>
2018-03-18 16:51:21 -0500

    More robust constructor (checking inputs)

--- ch12/vector.m
+++ ch12/vector.m
@@ -9,15 +9,17 @@ classdef vector
 	methods
 		function this = vector(a, b)
 			narginchk(0, 2);
-			if nargin == 2
+			if nargin == 2 && isnumeric(a) && isscalar(a) && isnumeric(b) && isscalar(b)
 				this.x = a;
 				this.y = b;
-			elseif nargin == 1
+			elseif nargin == 1 && isnumeric(a) && isscalar(a)
 				this.x = a;
 				this.y = 0;
-			else 
+			elseif nargin == 0
 				this.x = 0;
 				this.y = 0;
+			else
+				error('Incorrect actual arguments for vector. (Check the actual argument types.)');
 			end
 		end
 	end
