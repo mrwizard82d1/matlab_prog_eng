bf7c26122a5c2df209f1027d924e41cd532ffca5
Larry Jones <larry.jones@reveal-energy.com>
2018-03-18 16:59:02 -0500

    Add query methods: length and add

--- ch12/vector.m
+++ ch12/vector.m
@@ -7,20 +7,28 @@ classdef vector
 	end
 
 	methods
-		function this = vector(a, b)
+		function result = vector(a, b)
 			narginchk(0, 2);
 			if nargin == 2 && isnumeric(a) && isscalar(a) && isnumeric(b) && isscalar(b)
-				this.x = a;
-				this.y = b;
+				result.x = a;
+				result.y = b;
 			elseif nargin == 1 && isnumeric(a) && isscalar(a)
-				this.x = a;
-				this.y = 0;
+				result.x = a;
+				result.y = 0;
 			elseif nargin == 0
-				this.x = 0;
-				this.y = 0;
+				result.x = 0;
+				result.y = 0;
 			else
 				error('Incorrect actual arguments for vector. (Check the actual argument types.)');
 			end
+		end
+
+		function result = length(this)
+			result = sqrt(this.x .^ 2 + this.y .^ 2);
+		end
+
+		function result = add(this, another)
+			result = vector(this.x + another.x, this.y + another.y);
 		end
 	end
 end
