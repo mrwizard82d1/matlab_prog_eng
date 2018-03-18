ce6d45acb3eb36a220c574c4d6e114712b35a7e5
Larry Jones <larry.jones@reveal-energy.com>
2018-03-18 16:38:03 -0500

    More robust vector constructor

--- ch12/vector.m
+++ ch12/vector.m
@@ -1,16 +1,25 @@
 % A simple vector class
 
 classdef vector
-	methods
-		function this = vector(a, b)
-			this.x = a;
-			this.y = b;
-		end
-	end
-
 	properties
 		x; % x-value of vector
 		y; % y-value of vector
+	end
+
+	methods
+		function this = vector(a, b)
+			narginchk(0, 2);
+			if nargin == 2
+				this.x = a;
+				this.y = b;
+			elseif nargin == 1
+				this.x = a;
+				this.y = 0;
+			else 
+				this.x = 0;
+				this.y = 0;
+			end
+		end
 	end
 end
 
